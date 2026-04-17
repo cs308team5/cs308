6e5f8e428ffb3debbb78b920158fdc68821b482f
Ritiam <146821363+Ritiam@users.noreply.github.com>
2026-04-17 15:16:07 +0300

    homepage bugfix2

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import "./HomePage.css";
+//import "./HomePage.css";
 import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
