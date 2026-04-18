6a8af3956abca67cab441f54ac0f81faf23fbfa9
sudentrs <sude.tiras@sabanciuniv.edu>
2026-04-19 00:27:34 +0300

    added getproductdetails api

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getProducts } from "../controllers/productController.js";
+import { getProductById } from "../controllers/productController.js";
 import pool from "../config/db.js";
 
 const router = express.Router();
@@ -29,5 +30,7 @@ router.get("/search", async (req, res) => {
     res.status(500).json({ error: "Database error" });
   }
 });
+
+router.get("/:id", getProductById);
 
 export default router;
